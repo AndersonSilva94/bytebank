8eec81a7718c50498b9980aaff4f88049f74fc40
Anderson Silva <andssilva.23@gmail.com>
2022-08-20 13:03:45 -0400

    [nova-transferencia] feat: :rocket: exportando dados com output

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
@@ -16,7 +18,9 @@ export class NovaTransferenciaComponent implements OnInit {
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor)
-    console.log('Destino: ', this.destino)
+    this.aoTransferir.emit({
+      valor: this.valor,
+      destino: this.destino
+    })
   }
 }
